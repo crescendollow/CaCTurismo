1dd347bb2723a12133598ec357da96385c7c43cf
Senen Urdaneta <senenurdaneta@gmail.com>
2023-09-24 18:04:59 -0300

    Acutalizar Reservas

--- TurismoCaC/TurismoCaC/core/urls.py
+++ TurismoCaC/TurismoCaC/core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hotel/list', views.hotel_list, name='hotel_list'),
-    re_path(r'listado/reservas/(?P<dia>[0-9]{2})/(?P<mes>[0-9]{2})/(?P<anio>[0-9]{4})/$', views.listado_reservas_fecha, name='listado_reservas'),
+    re_path(r'listado/reservas/(?P<dia>[0-9]{2})/(?P<mes>[0-9]{2})/(?P<anio>[0-9]{4})/$', views.listado_reservas_fecha, name='listado_reservas_fecha'),
     path('listado/reservas', views.listado_reservas, name='listado_reservas')
 
 ]
