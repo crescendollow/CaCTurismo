daea7c09745c21fe3e4a8a0e95980968dd37653a
Senen Urdaneta <senenurdaneta@gmail.com>
2023-10-22 23:25:31 -0300

    From Reserva

--- TurismoCaC/TurismoCaC/core/forms.py
+++ TurismoCaC/TurismoCaC/core/forms.py
@@ -13,3 +13,15 @@ class ClienteForm(forms.Form):
         if self.cleaned_data["mail"] == "cac@cac.com":
             raise ValidationError("El correo ya existe")
         return self.cleaned_data
+
+
+class ReservasForm(forms.Form):
+    fecha_reservas = forms.DateField(label="Fecha de Inicio de Reserva:", required=True)
+    fecha_salida = forms.DateField(label="Fecha de Salida de Reserva:", required=True)
+    cant_personas = forms.IntegerField(label="Cantidad de Personas:", required=True)
+    mensaje =  forms.CharField(label="Observacion:", widget=forms.Textarea)
+
+    # def clean(self):
+    #     if self.cleaned_data["fecha_salida"] > self.cleaned_data["fecha_reservas"]:
+    #         raise ValidationError("La Fecha de Salida tiene que ser Mayor que la fecha de Reserva")
+    #     return self.cleaned_data
--- TurismoCaC/TurismoCaC/core/urls.py
+++ TurismoCaC/TurismoCaC/core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     re_path(r'listado/reservas/(?P<dia>[0-9]{2})/(?P<mes>[0-9]{2})/(?P<anio>[0-9]{4})/$', views.listado_reservas_fecha, name='listado_reservas_fecha'),
     path('listado/reservas', views.listado_reservas, name='listado_reservas'),
     path('cliente', views.cliente, name="cliente"),
+    path('reservas', views.reservas, name="reservas"),
     
 ]
