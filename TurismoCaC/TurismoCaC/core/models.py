f2e129dbda4051974a488b1a1d7344edbf30fc25
Senen Urdaneta <senenurdaneta@gmail.com>
2023-10-23 22:47:38 -0300

    Creacion de Models Reserva

--- TurismoCaC/TurismoCaC/core/models.py
+++ TurismoCaC/TurismoCaC/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+class reservas(models.Model):
+    fecha_reservas = models.DateField(null=False, verbose_name="fecha_reservas")
+    fecha_salida = models.DateField(null=False, verbose_name="fecha_salida")
+    cant_personas = models.IntegerField(verbose_name="cant_personas")
+    mensaje =  models.CharField(max_length=150, null=True, verbose_name="mensaje")
